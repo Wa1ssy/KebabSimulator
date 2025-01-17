c9fba784638786f8c223ed9728c4ebc851929839
opilane <opilane@HP-CZC3217070>
2025-01-17 12:51:36 +0200

    emailcontrollerisse lisatud tokeni jaoks httppost

--- Kebab Simulator/Controllers/EmailsController.cs
+++ Kebab Simulator/Controllers/EmailsController.cs
@@ -31,5 +31,18 @@ namespace Kebab_Simulator.Controllers
             _emailsServices.SendEmail(dto);
             return RedirectToAction(nameof(Index));
         }
+        [HttpPost]
+        public IActionResult SendTokenEmail(EmailViewModel viewModel, string token)
+        {
+            var dto = new EmailTokenDto()
+            {
+                To = viewModel.To,
+                Subject = viewModel.Subject,
+                Body = viewModel.Body,
+                Token = token
+            };
+            _emailsServices.SendEmailToken(dto, token);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
---
+++ Kebab Simulator/Models/TagHelpers/PanelTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Kebab_Simulator.TagHelpers
+{
+    [HtmlTargetElement("panel")]
+    public class PanelTagHelper : TagHelper
+    {
+        public string Title { get; set; } //title of the panel
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "panel");
+
+            var content = await output.GetChildContentAsync();
+
+            var panelHtml = "test";
+
+            output.Content.SetHtmlContent(panelHtml);
+        }
+    }
+}
