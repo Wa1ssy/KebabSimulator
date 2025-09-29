3e91a59f8b2828688768070916e5bf0b396c0c46
Artur <opilane@HP-CZC321705F>
2025-09-29 09:27:20 +0300

    GameViewModel pooleli tehtud, Allan peab tööd ka tegema(https://trello.com/c/jqR60HyN/14-create-gameviewmodel)

---
+++ Kebab Simulator.ApplicationServices/Services/GameService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KebabSimulator.ApplicationServices
+{
+    public interface IGameService
+    {
+        event EventHandler<int> ScoreChanged;
+        void Start();
+        void Pause();
+        void AddScore(int delta);
+    }
+
+    public class GameService : IGameService
+    {
+        private int _score;
+        public event EventHandler<int> ScoreChanged;
+
+        public void Start()
+        {
+            _score = 0;
+            OnScoreChanged();
+        }
+
+        public void Pause()
+        {
+            // siia saad panna loogika mängu pausile panemiseks
+        }
+
+        public void AddScore(int delta)
+        {
+            _score += delta;
+            OnScoreChanged();
+        }
+
+        private void OnScoreChanged()
+        {
+            ScoreChanged?.Invoke(this, _score);
+        }
+    }
+}
---
+++ Kebab Simulator/ViewModels/GameViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace KebabSimulator.ViewModels
+{
+    public class GameViewModel : INotifyPropertyChanged
+    {
+        private readonly IGameService _gameService;
+        private int _score;
+        private bool _isRunning;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public GameViewModel(IGameService gameService)
+        {
+            _gameService = gameService;
+
+            StartCommand = new RelayCommand(StartGame, () => !IsRunning);
+            PauseCommand = new RelayCommand(PauseGame, () => IsRunning);
+
+            _gameService.ScoreChanged += OnScoreChanged;
+        }
+
+        public ICommand StartCommand { get; }
+        public ICommand PauseCommand { get; }
+
+        public int Score
+        {
+            get => _score;
+            private set
+            {
+                if (_score != value)
+                {
+                    _score = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            private set
+            {
+                if (_isRunning != value)
+                {
+                    _isRunning = value;
+                    OnPropertyChanged();
+                    ((RelayCommand)StartCommand).RaiseCanExecuteChanged();
+                    ((RelayCommand)PauseCommand).RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        private void StartGame()
+        {
+            _gameService.Start();
+            IsRunning = true;
+        }
+
+        private void PauseGame()
+        {
+            _gameService.Pause();
+            IsRunning = false;
+        }
+
+        private void OnScoreChanged(object sender, int newScore)
+        {
+            Score = newScore;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
