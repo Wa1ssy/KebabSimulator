7d9f1c4f88ac3920d36962ce17d930a2370b39e0
Wa1ssy <brawlstarstop2289@gmail.com>
2025-11-13 14:45:41 +0200

    Added files for health inspection pop up

---
+++ Kebab Simulator.Core/Services/HealthInspectionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kebab_Simulator.Core.Services
+{
+    internal class HealthInspectionService
+    {
+    }
+}
---
+++ Kebab Simulator/Controllers/BaseKebabController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kebab_Simulator.Controllers
+{
+    public class BaseKebabController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
