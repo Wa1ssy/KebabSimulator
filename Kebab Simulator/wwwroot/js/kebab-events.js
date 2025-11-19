292555267ea93c52191bb1a3c1be838bd058e472
villemmannard <villemmannard@gmail.com>
2025-11-19 16:35:55 +0200

    Connected the cockroach and health inspection function together ( i hope it works) and added testing project + started writing test

---
+++ Kebab Simulator/wwwroot/js/kebab-events.js
@@ -0,0 +1,23 @@
+﻿window.cockroachOnScreen = false;
+
+function spawnCockroach() {
+    window.cockroachOnScreen = true;
+}
+function killCockroachClient() {
+    window.cockroachOnScreen = false;
+}
+
+function onCockroachClicked() {
+
+    fetch('/Kebab/KillCockroach', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: '{}'
+    })
+        .then(response => response.json())
+        .then(data => {
+            killCockroachClient();
+        });
+}
