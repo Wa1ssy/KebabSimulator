cb4059522934bece7019d5b3440b89cb3e639463
Wa1ssy <brawlstarstop2289@gmail.com>
2025-11-20 14:58:28 +0200

    Update HealthInspectionService.cs

--- Kebab Simulator.Core/Services/HealthInspectionService.cs
+++ Kebab Simulator.Core/Services/HealthInspectionService.cs
@@ -5,7 +5,7 @@ namespace Kebab_Simulator.Core.Services
     public static class HealthInspectionService
     {
         private static readonly Random _random = new Random();
-        public static bool ShouldShowHealthInspection(double chance = 10.0)
+        public static bool ShouldShowHealthInspection(double chance = 0.2)
         {
             return _random.NextDouble() < chance;
         }
